f0cc71840d65485ecdf150bce574038f8c9dad9f
Percy Pacco <777percy@gmail.com>
2012-10-20 22:08:44 -0500

    comin 01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+New02::Application.config.secret_token = 'c6fe395c548c1c0dc34125fe407607123975c2c1fdf16a68e332c2b4d1025bb18b0356e2140d3f9b9573286fde757043b9f82164c9d3029636eb6c9c46c7d92b'
